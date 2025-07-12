2a9872d1f5daf2a55cdc90c4529c0800c5a69f20
vamsi <vamsichinnu537@gmail.com>
2025-07-12 23:14:28 +0530

    Update tokenizer.hpp

--- phase-1/tokenizer.hpp
+++ phase-1/tokenizer.hpp
@@ -1,25 +1,25 @@
-#include <fstream>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <vector>
 
+// tokenizer.hpp
 #pragma once
 #include <clang-c/Index.h>
-
-// You should NOT modify ANYTHING in this file.
+#include <string>
+#include <vector>
 
 class tokenizer_t {
 public:
     tokenizer_t(std::string __file_name);
-    ~tokenizer_t(void);
-    std::vector<int> get_tokens(void);
+    ~tokenizer_t();
 
-protected:
+    std::vector<int> get_tokens();      // Just the kinds
+    void print_tokens();                // Prints <kind> - <spelling> <line> <column>
+
+private:
     std::string file_name;
     CXTranslationUnit unit;
     CXIndex index;
+
     bool is_from_main_file(CXCursor __cursor);
+
     struct tokenizer_data_t {
         std::vector<int> tokens;
         tokenizer_t* tokenizer;
